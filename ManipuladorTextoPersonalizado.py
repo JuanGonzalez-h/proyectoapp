dbb8d092ca087bf557eecfc0270c3d19accdf9d1
garciaarboledalaura <garciaarboledalaura@users.noreply.github.com>
2025-11-09 01:22:04 -0500

    commit Laura

--- ManipuladorTextoPersonalizado.py
+++ ManipuladorTextoPersonalizado.py
@@ -2,27 +2,21 @@ from ManipuladorTexto import ManipuladorTexto
 
 class ManipuladorTextoPersonalizado(ManipuladorTexto):
     
-    def _init_(self, estiloFormal):
-        ManipuladorTexto._init_(self)
+    def __init__(self, estiloFormal=True):
+        super().__init__()
         self.estiloFormal = estiloFormal
     
-    def manipuladoTexto(self, texto):
-        
-        if self.estiloFormal:
-           return texto
-        else:
-         return texto
-    
-    def formatearNotificacion(self, amigo):
-        nombre = amigo.obtenerNombre()
+def formatearNotificacion(self, amigo):
+        nombre = amigo.nombre
         if self.estiloFormal:
             return "Estimado usuario, le recordamos contactar a: " + nombre
         else:
             return "Hey! No olvides hablar con " + nombre + " 😊"
     
-    def cambiarEstiloFormal(self, nuevo_estilo):
+def cambiarEstiloFormal(self, nuevo_estilo):
         self.estiloFormal = nuevo_estilo
         if nuevo_estilo:
             return "Estilo cambiado a: Formal"
         else:
             return "Estilo cambiado a: Informal"
+        
